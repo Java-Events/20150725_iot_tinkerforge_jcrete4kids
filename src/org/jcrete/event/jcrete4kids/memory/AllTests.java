ac5ba5a8c87b52bcf91888161e17649294a932b9
Jan Wloka <jan.wloka@quatico.com>
2015-07-29 12:04:39 +0200

    Share run configurations for memory game.

---
+++ src/org/jcrete/event/jcrete4kids/memory/AllTests.java
@@ -0,0 +1,9 @@
+package org.jcrete.event.jcrete4kids.memory;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ MemoryGameTest.class })
+public class AllTests {
+}
